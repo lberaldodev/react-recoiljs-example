4cbb3adb75cf49f6357cb44ee47d236fc836920b
Lucas Beraldo <lberaldo01@gmail.com>
2020-09-24 23:44:45 -0300

    feat: remove files not necessary and add input char counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRecoilState, useRecoilValue} from 'recoil'
+import { inputValue, charCountState } from './atoms/inputValue'
 
 function App() {
+
+  const [value, setInputValue] = useRecoilState(inputValue);
+  
+  const charactersCounter = useRecoilValue(charCountState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={(e) => setInputValue(e.target.value)} value={value} />
+      <div>
+        {charactersCounter}
+      </div>
     </div>
   );
 }
